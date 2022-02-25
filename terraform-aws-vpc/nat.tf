9955f05f34c6674af2882ab89f7d51a25dd6e094
Sukanya1201 <83268513+Sukanya1201@users.noreply.github.com>
2022-02-25 14:31:27 +0530

    Update nat.tf

--- terraform-aws-vpc/nat.tf
+++ terraform-aws-vpc/nat.tf
@@ -6,6 +6,7 @@ resource "aws_nat_gateway" "nat_gw" {
   allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.public_1.id}"
   depends_on    = ["aws_internet_gateway.gw"]
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_route_table" "route_private" {
