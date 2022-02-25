9165521ccc667e780038719c37cce668a480bed6
Sukanya1201 <83268513+Sukanya1201@users.noreply.github.com>
2022-02-25 14:52:17 +0530

    Update route_table.tf

--- terraform-aws-vpc/route_table.tf
+++ terraform-aws-vpc/route_table.tf
@@ -21,7 +21,22 @@ resource "aws_route_table_association" "public_2" {
   route_table_id = "${aws_route_table.route-public.id}"
 }
 
-resource "aws_route_table_association" "public_3" {
-  subnet_id      = "${aws_subnet.public_3.id}"
-  route_table_id = "${aws_route_table.route-public.id}"
+resource "aws_route_table" "route-private" {
+  vpc_id = "${aws_vpc.vpc_demo.id}"
+
+ 
+
+  tags = {
+    Name = "private-route-table-demo"
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = "${aws_subnet.private_1.id}"
+  route_table_id = "${aws_route_table.route-private.id}"
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = "${aws_subnet.private_2.id}"
+  route_table_id = "${aws_route_table.route-private.id}"
 }
